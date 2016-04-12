85e700ed449f3b20ddca098fafbfa67aecd8b010
caponica <github@caponica.com>
2016-04-13 00:55:14 +0100

    First pass at namespaced ProductClient

---
+++ src/CaponicaAmazonMwsComplete/MwsProductClient.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace CaponicaAmazonMwsComplete;
+
+require_once __DIR__.'/../AmazonPhpClientLibrary/MarketplaceWebServiceProducts/Client.php';
+
+class MwsProductClient extends \MarketplaceWebServiceProducts_Client {
+
+}
