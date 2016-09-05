8c856bf801df08dc02d51f9811e50fc6d8067929
caponica <github@caponica.com>
2016-09-05 02:48:18 +0100

    When bouncing off throttle limit, increase snooze time to a minimum of 2s

--- src/CaponicaAmazonMwsComplete/ClientPack/CaponicaClientPack.php
+++ src/CaponicaAmazonMwsComplete/ClientPack/CaponicaClientPack.php
@@ -14,7 +14,7 @@ class CaponicaClientPack {
             if (method_exists($e, 'getErrorCode') && 'RequestThrottled' == $e->getErrorCode()) {
                 echo "\nThe request was throttled";
                 if ($snoozeLength = $clientPack->getThrottleManager()->getRestoreInterval($method, $weight)) {
-                    self::snooze($snoozeLength * 2); // Double the normal snooze since we bounced off the server limit
+                    self::snooze(ceil($snoozeLength) * 2); // Double the normal snooze since we bounced off the server limit
 
                     // try again. If there's another exception it will bubble up to the caller.
                     return $clientPack->$method($options);
