94e9e1c65ff3d76a13c8578ca90e40bff45ff399
caponica <github@caponica.com>
2016-12-06 09:29:38 +0000

    Fixed structure of extra parameters in callGetReportRequestList()

--- src/CaponicaAmazonMwsComplete/ClientPack/MwsFeedAndReportClientPack.php
+++ src/CaponicaAmazonMwsComplete/ClientPack/MwsFeedAndReportClientPack.php
@@ -293,25 +293,27 @@ class MwsFeedAndReportClientPack extends MwsFeedAndReportClient {
     }
 
     /**
-     * @param string|array $reportRequestIds    One or more ReportRequestIds, as returned by requestReport()
-     * @param array $reportTypeList                 (optional) An array of ReportTypes to fetch
-     * @param array $reportProcessingStatusList     (optional) An array of ReportStatuses to fetch
+     * @param string|array $reportRequestIds        One or more ReportRequestIds, as returned by requestReport()
+     * @param array $reportTypes                    (optional) An array of ReportTypes to fetch
+     * @param array $reportProcessingStatuses       (optional) An array of ReportStatuses to fetch
      * @param \DateTime $requestedFromDate          (optional) The start of a date range used for selecting the data to report
      * @param \DateTime $requestedToDate            (optional) The end of a date range used for selecting the data to report
      * @param int $maxCount                         (optional) The max number of report requests to return
      * @return \MarketplaceWebService_Model_GetReportRequestListResponse
      */
-    public function callGetReportRequestList($reportRequestIds, $reportTypeList=[], $reportProcessingStatusList=[], $requestedFromDate=null, $requestedToDate=null, $maxCount=10) {
+    public function callGetReportRequestList($reportRequestIds, $reportTypes=[], $reportProcessingStatuses=[], $requestedFromDate=null, $requestedToDate=null, $maxCount=10) {
         $parameters = [
             self::PARAM_MARKETPLACE             => $this->marketplaceId,
             self::PARAM_MERCHANT                => $this->sellerId,
-            self::PARAM_REPORT_REQUEST_ID_LIST  => array('Id' => $reportRequestIds),
         ];
-        if (!empty($reportTypeList)) {
-            $parameters[self::PARAM_REPORT_TYPE_LIST] = $reportTypeList;
+        if (!empty($reportRequestIds)) {
+            $parameters[self::PARAM_REPORT_REQUEST_ID_LIST] = ['Id' => $reportRequestIds];
         }
-        if (!empty($reportProcessingStatusList)) {
-            $parameters[self::PARAM_REPORT_PROCESSING_STATUS_LIST] = $reportProcessingStatusList;
+        if (!empty($reportTypes)) {
+            $parameters[self::PARAM_REPORT_TYPE_LIST] = ['Type' => $reportTypes];
+        }
+        if (!empty($reportProcessingStatuses)) {
+            $parameters[self::PARAM_REPORT_PROCESSING_STATUS_LIST] = ['Status' => $reportProcessingStatuses];
         }
         if (!empty($requestedFromDate)) {
             $parameters[self::PARAM_REQUESTED_FROM_DATE] = $requestedFromDate;
