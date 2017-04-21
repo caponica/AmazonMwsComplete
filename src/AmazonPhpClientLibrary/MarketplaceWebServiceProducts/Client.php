656d7487f61d95a0955a49405d58eb1324c3219e
Rahul Kadyan <hi@znck.me>
2017-04-21 17:59:27 +0530

    Remove iconv_set_encoding for PHP 5.6+

--- src/AmazonPhpClientLibrary/MarketplaceWebServiceProducts/Client.php
+++ src/AmazonPhpClientLibrary/MarketplaceWebServiceProducts/Client.php
@@ -896,9 +896,11 @@ class MarketplaceWebServiceProducts_Client implements MarketplaceWebServiceProdu
      */
     public function __construct($awsAccessKeyId, $awsSecretAccessKey, $applicationName, $applicationVersion, $config = null)
     {
-        iconv_set_encoding('output_encoding', 'UTF-8');
-        iconv_set_encoding('input_encoding', 'UTF-8');
-        iconv_set_encoding('internal_encoding', 'UTF-8');
+        if (PHP_VERSION_ID < 50600) {
+            iconv_set_encoding('output_encoding', 'UTF-8');
+            iconv_set_encoding('input_encoding', 'UTF-8');
+            iconv_set_encoding('internal_encoding', 'UTF-8');
+        }
 
         $this->_awsAccessKeyId = $awsAccessKeyId;
         $this->_awsSecretAccessKey = $awsSecretAccessKey;
