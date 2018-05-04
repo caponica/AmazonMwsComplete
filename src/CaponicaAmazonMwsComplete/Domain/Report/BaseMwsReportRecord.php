6db905eae6f5e7cee0dbacebce6f4d91f8004409
caponica <github@caponica.com>
2018-05-04 14:10:15 +0100

    Clean up quotation marks in source record files (Amazon format is (tab)"abc\"def\"ghi"(tab) and we clean it up to (tab)abc"def"ghi(tab)

--- src/CaponicaAmazonMwsComplete/Domain/Report/BaseMwsReportRecord.php
+++ src/CaponicaAmazonMwsComplete/Domain/Report/BaseMwsReportRecord.php
@@ -13,6 +13,17 @@ abstract class BaseMwsReportRecord {
             throw new InvalidReportRecordException("Expected $expectedFieldCount fields, but found $fieldCount in row: [ " . implode('~~~', $fieldArray) . " ]");
         }
 
+        array_walk($fieldArray, [$this, 'cleanQuotes']);
+
         return $fieldArray;
+    }
+
+    protected function cleanQuotes(&$string, $key) {
+        if (is_string($string) && substr($string, 0, 1) === '"' && substr($string, -1, 1) === '"') {
+            if (strpos($string, '\"') !== false) {
+                $string = str_replace('\"', '"', $string);
+            }
+            $string = substr($string, 1, -1);
+        }
     }
 }
