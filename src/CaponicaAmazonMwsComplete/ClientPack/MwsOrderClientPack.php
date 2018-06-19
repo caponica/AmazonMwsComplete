cd676d821ba957c852e24574a9178ea3bc52799d
Jason Hebert <jason.hebert@powersrc.net>
2018-06-19 18:51:51 -0500

    Add missing Order methods
    Add missing `callListOrderItems()` and `callListOrderItemsByNextToken()` methods.

--- src/CaponicaAmazonMwsComplete/ClientPack/MwsOrderClientPack.php
+++ src/CaponicaAmazonMwsComplete/ClientPack/MwsOrderClientPack.php
@@ -2,41 +2,43 @@
 
 namespace CaponicaAmazonMwsComplete\ClientPack;
 
-use CaponicaAmazonMwsComplete\ClientPool\MwsClientPoolConfig;
 use CaponicaAmazonMwsComplete\AmazonClient\MwsOrderClient;
+use CaponicaAmazonMwsComplete\ClientPool\MwsClientPoolConfig;
 use CaponicaAmazonMwsComplete\Domain\Throttle\ThrottleAwareClientPackInterface;
 use CaponicaAmazonMwsComplete\Domain\Throttle\ThrottledRequestManager;
 
 class MwsOrderClientPack extends MwsOrderClient implements ThrottleAwareClientPackInterface {
-    const PARAM_AMAZON_ORDER_IDS            = 'AmazonOrderId';
-    const PARAM_CREATED_AFTER               = 'CreatedAfter';
-    const PARAM_CREATED_BEFORE              = 'CreatedBefore';
-    const PARAM_LAST_UPDATED_AFTER          = 'LastUpdatedAfter';
-    const PARAM_LAST_UPDATED_BEFORE         = 'LastUpdatedBefore';
-    const PARAM_MARKETPLACE_ID              = 'MarketplaceId';
-    const PARAM_MARKETPLACE_ID_LIST         = 'MarketplaceId.Id.1';
-    const PARAM_MWS_AUTH_TOKEN              = 'MWSAuthToken';
-    const PARAM_MERCHANT                    = 'SellerId';
-    const PARAM_SELLER_ID                   = 'SellerId';   // Alias for PARAM_MERCHANT
-    const PARAM_NEXT_TOKEN                  = 'NextToken';
-    const PARAM_ORDER_STATUS_LIST           = 'OrderStatus';
+    const PARAM_AMAZON_ORDER_IDS                = 'AmazonOrderId';
+    const PARAM_CREATED_AFTER                   = 'CreatedAfter';
+    const PARAM_CREATED_BEFORE                  = 'CreatedBefore';
+    const PARAM_LAST_UPDATED_AFTER              = 'LastUpdatedAfter';
+    const PARAM_LAST_UPDATED_BEFORE             = 'LastUpdatedBefore';
+    const PARAM_MARKETPLACE_ID                  = 'MarketplaceId';
+    const PARAM_MARKETPLACE_ID_LIST             = 'MarketplaceId.Id.1';
+    const PARAM_MWS_AUTH_TOKEN                  = 'MWSAuthToken';
+    const PARAM_MERCHANT                        = 'SellerId';
+    const PARAM_SELLER_ID                       = 'SellerId';   // Alias for PARAM_MERCHANT
+    const PARAM_NEXT_TOKEN                      = 'NextToken';
+    const PARAM_ORDER_STATUS_LIST               = 'OrderStatus';
 
-    const STATUS_PENDING_AVAILABILITY       = 'PendingAvailability';
-    const STATUS_PENDING                    = 'Pending';
-    const STATUS_UNSHIPPED                  = 'Unshipped';
-    const STATUS_PARTIALLY_SHIPPED          = 'PartiallyShipped';
-    const STATUS_SHIPPED                    = 'Shipped';
-    const STATUS_INVOICE_UNCONFIRMED        = 'InvoiceUnconfirmed';
-    const STATUS_CANCELED                   = 'Canceled';
-    const STATUS_UNFULFILLABLE              = 'Unfulfillable';
+    const STATUS_PENDING_AVAILABILITY           = 'PendingAvailability';
+    const STATUS_PENDING                        = 'Pending';
+    const STATUS_UNSHIPPED                      = 'Unshipped';
+    const STATUS_PARTIALLY_SHIPPED              = 'PartiallyShipped';
+    const STATUS_SHIPPED                        = 'Shipped';
+    const STATUS_INVOICE_UNCONFIRMED            = 'InvoiceUnconfirmed';
+    const STATUS_CANCELED                       = 'Canceled';
+    const STATUS_UNFULFILLABLE                  = 'Unfulfillable';
 
-    const METHOD_GET_ORDER                  = 'getOrder';
-    const METHOD_LIST_ORDERS                = 'listOrders';
-    const METHOD_LIST_ORDERS_BY_NEXT_TOKEN  = 'listOrdersByNextToken';
+    const METHOD_GET_ORDER                      = 'getOrder';
+    const METHOD_LIST_ORDERS                    = 'listOrders';
+    const METHOD_LIST_ORDERS_BY_NEXT_TOKEN      = 'listOrdersByNextToken';
+    const METHOD_LIST_ORDER_ITEMS               = 'listOrderItems';
+    const METHOD_LIST_ORDER_ITEMS_BY_NEXT_TOKEN = 'listOrderItemsByNextToken';
 
-    const OPTION_MARKETPLACE_ONLY_AMAZON    = 'AMZ';
-    const OPTION_MARKETPLACE_ONLY_NON_AMAZON= 'MCF';
-    const OPTION_MARKETPLACE_ALL            = 'ALL';
+    const OPTION_MARKETPLACE_ONLY_AMAZON        = 'AMZ';
+    const OPTION_MARKETPLACE_ONLY_NON_AMAZON    = 'MCF';
+    const OPTION_MARKETPLACE_ALL                = 'ALL';
 
     /** @var string $marketplaceId              The MWS MarketplaceID string used in API connections (for Amazon orders) */
     protected $marketplaceId;
@@ -142,6 +144,24 @@ class MwsOrderClientPack extends MwsOrderClient implements ThrottleAwareClientPa
         return CaponicaClientPack::throttledCall($this, self::METHOD_LIST_ORDERS_BY_NEXT_TOKEN, $requestArray);
     }
 
+    public function callListOrderItems($amazonOrderId)
+    {
+        $requestArray = [self::PARAM_AMAZON_ORDER_IDS => $amazonOrderId];
+
+        $requestArray = $this->signArray($requestArray);
+
+        return CaponicaClientPack::throttledCall($this, self::METHOD_LIST_ORDER_ITEMS, $requestArray);
+    }
+
+    public function callListOrderItemsByNextToken($nextToken)
+    {
+        $requestArray = [self::PARAM_NEXT_TOKEN => $nextToken];
+
+        $requestArray = $this->signArray($requestArray);
+
+        return CaponicaClientPack::throttledCall($this, self::METHOD_LIST_ORDER_ITEMS_BY_NEXT_TOKEN, $requestArray);
+    }
+
     private function convertMarketplaceOptionIntoArray($marketplaceOption) {
         if (empty($marketplaceOption) || self::OPTION_MARKETPLACE_ONLY_AMAZON === $marketplaceOption) {
             return $this->marketplaceId;
@@ -163,9 +183,11 @@ class MwsOrderClientPack extends MwsOrderClient implements ThrottleAwareClientPa
     public function initThrottleManager() {
         $this->throttleManager = new ThrottledRequestManager(
             [
-                self::METHOD_GET_ORDER                  => [6, 0.015],
-                self::METHOD_LIST_ORDERS                => [6, 0.015],
-                self::METHOD_LIST_ORDERS_BY_NEXT_TOKEN  => [null, null, null, self::METHOD_LIST_ORDERS],
+                self::METHOD_GET_ORDER                      => [6, 0.015],
+                self::METHOD_LIST_ORDERS                    => [6, 0.015],
+                self::METHOD_LIST_ORDERS_BY_NEXT_TOKEN      => [null, null, null, self::METHOD_LIST_ORDERS],
+                self::METHOD_LIST_ORDER_ITEMS               => [30, 0.5],
+                self::METHOD_LIST_ORDER_ITEMS_BY_NEXT_TOKEN => [30, 0.5],
             ]
         );
     }
